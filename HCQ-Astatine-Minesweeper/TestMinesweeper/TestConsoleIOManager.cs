ab90385536a7bc06a9dd842ac3385fafe341d335
Ventsislav Staykov <vencislav.staykov@gmail.com>
2013-05-16 23:57:28 +0300

    Test classes added
    * Test ConsoleIOManager
    * Test ScoreBoard

---
+++ HCQ-Astatine-Minesweeper/TestMinesweeper/TestConsoleIOManager.cs
@@ -0,0 +1,27 @@
+﻿namespace TestMinesweeper
+{
+    using System;
+    using Minesweeper;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestConsoleIOManager
+    {
+        [TestMethod]
+        public void TestInitialMessage()
+        {
+            string welcomeMessage = @"Welcome to the game “Minesweeper”. Try to reveal all cells without mines.";
+            string commands = "Use 'top' to view the scoreboard, 'restart' to start a new game and 'exit' to quit  the game.";
+            string outputMessage = ConsoleIOManager.PrintInitialMessage();
+            Assert.AreEqual(outputMessage,(welcomeMessage + " " + commands));
+        }
+
+        //[TestMethod]
+        //public void TestUserInput()
+        //{
+        //    string input = "2, 3";
+        //    string checkInput = ConsoleIOManager.GetUserInput();
+        //    Assert.AreEqual(input, checkInput);
+        //}
+    }
+}
---
+++ HCQ-Astatine-Minesweeper/TestMinesweeper/TestScoreBoard.cs
@@ -0,0 +1,34 @@
+﻿namespace TestMinesweeper
+{
+    using System;
+    using Minesweeper;
+    using Wintellect.PowerCollections;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestScoreBoard
+    {
+        [TestMethod]
+        public void TestAddPlayerInScoreBoard()
+        {
+            OrderedMultiDictionary<int, string> orderedMultiDictionary = new OrderedMultiDictionary<int, string>(true);
+            orderedMultiDictionary.Add(10, "Pesho");
+            string playerName = "Pesho";
+            int playerScore = 10;
+            ScoreBoard scoreBoard = new ScoreBoard();
+            scoreBoard.AddPlayer(playerName, playerScore);
+            Assert.AreEqual(scoreBoard, orderedMultiDictionary);
+        }
+
+        [TestMethod]
+        public void TestGenerateScoreBoard()
+        {
+            string output = "\n\rScoreboard:\n\r1. Pesho --> 10 cells\n";
+            ScoreBoard scoreBoard = new ScoreBoard();
+            string playerName = "Pesho";
+            int playerScore = 10;
+            scoreBoard.AddPlayer(playerName, playerScore);
+            Assert.AreEqual(output, scoreBoard.Generate());
+        }
+    }
+}
